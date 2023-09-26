84637856fccbad836afb1f386a3294141cd69f1a
dydfuf <>
2023-09-26 16:30:19 +0000

    몇가지 패키지 버전을 업그레이드 및 불필요 코드를 제거합니다 (#58)
    * 몇가지 패키지 버전을 업그레이드 합니다
    
    * 불필요 코드 제거

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        {/** @TODO: https://mapleboss.atlassian.net/browse/MAW-41
-         * prod 배포시 해당 태그를 삭제한다.
-         */}
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
