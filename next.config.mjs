df10cfc6d5b06e0c0b001cb043c0a876d8d0f564
dydfuf <>
2023-09-27 10:56:58 +0000

    루트페이지를 파티로 리다이렉트 합니다 (#61)

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
-import withBundleAnalyzer from "@next/bundle-analyzer"
-import withPlugins from "next-compose-plugins"
-import { env } from "./env.mjs"
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import withPlugins from "next-compose-plugins";
+import { env } from "./env.mjs";
 
 /**
  * @type {import('next').NextConfig}
  */
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-    generateBuildId: async () => {
-        // You can, for example, get the latest git commit hash here
-        return crypto.randomUUID()
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return crypto.randomUUID();
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/party",
+        permanent: true,
       },
-})
+    ];
+  },
+});
 
-export default config
+export default config;
