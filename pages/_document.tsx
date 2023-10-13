673fd73d389540e37c27a3c2764aa5ee2b0ce3e6
dydfuf <>
2023-10-13 15:50:09 +0000

    [MAW-102] 구글 애드센스 관련 코드를 추가합니다. (#76)
    * 애드센스 메타태그를 추가합니다.
    
    * 파티리스트에 슬롯 광고를 추가합니다.
    
    * ads.txt 파일 추가
    
    * 파티리스트 애드슬롯 제거

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <meta
+          name="google-adsense-account"
+          content="ca-pub-3990660420952319"
+        ></meta>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3990660420952319"
+          crossOrigin="anonymous"
+        ></script>
+        <meta
           name="google-site-verification"
           content="4NGdq70g4vVxV9sIXHXilXj40YpBU2zGlrDfttr5uGg"
         />
