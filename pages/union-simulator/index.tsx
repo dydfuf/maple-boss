322fe5bfeca707c363d818d947382c3323c6ec4c
dydfuf <>
2024-01-05 16:17:44 +0000

    유니온 계산기 pagetitle 추가합니다.

--- pages/union-simulator/index.tsx
+++ pages/union-simulator/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
 import UnionSimulatorContainer from "components/UnionSimulator/UnionSimulatorContainer";
+import { getPageTitle } from "utils/meta";
 
 export default function UnionSimulatorPage() {
-  return <UnionSimulatorContainer />;
+  return (
+    <>
+      <Head>
+        <title>{getPageTitle("유니온 계산기")}</title>
+      </Head>
+      <UnionSimulatorContainer />
+    </>
+  );
 }
